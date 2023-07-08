27932d06620aff9b2d5a0168216c7577007f6c08
Nmaster88 <nunomaster21@gmail.com>
2023-07-08 14:45:54 +0100

    Update ConvertService.cs

--- CsvReaderApp.Service/ConvertService.cs
+++ CsvReaderApp.Service/ConvertService.cs
@@ -12,26 +12,8 @@ namespace CsvReaderApp.Services
         {
             public required string Name { get; set; }
             public required string Type { get; set; }
-
         }
         
-        public void Execute<T>(T reportOrigin) 
-        {
-            Type type = typeof(T);
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
-            {
-                // T is of type List<T> or its derived types
-                Console.WriteLine("T is of type List<T> or its derived types.");
-
-                IterateGenericTypeRead(reportOrigin, type);
-            }
-            else
-            {
-                // T is not of type List<T> or its derived types
-                Console.WriteLine("T is not of type List<T> or its derived types.");
-            }
-        }
-
         public void ExecuteAssignment<TI,TO>(TI reportOrigin, TO reportDestiny)
         {
             GenericInputTypeRead(reportOrigin);
@@ -67,7 +49,10 @@ namespace CsvReaderApp.Services
 
             if (TypeIsList(type))
             {
-                PropertyInfo[] properties = type.GetGenericArguments()[0].GetProperties();
+                Type typeOfList = type.GetGenericArguments()[0];
+                string typeName = typeOfList.Name;
+                Console.WriteLine($"Class name: {typeName}",typeName);
+                PropertyInfo[] properties = typeOfList.GetProperties();
 
                 foreach (PropertyInfo property in properties)
                 {
@@ -79,77 +64,13 @@ namespace CsvReaderApp.Services
                     };
                     ObjectProperties.Add(objProperty);
                 }
+                Console.WriteLine();
             }
             else
             {
                 Console.WriteLine($"{type.Name} is not of type List<> or its derived types.");
             }
 
-        }
-
-        private void IterateGenericTypeRead<T>(T report, Type type)
-        {
-            // Get the IEnumerable<T> interface
-            Type enumerableType = typeof(List<>).MakeGenericType(type.GetGenericArguments());
-
-            // Check if the report object implements IEnumerable<T>
-            if (enumerableType.IsAssignableFrom(report.GetType()))
-            {
-                MethodInfo getEnumeratorMethod = enumerableType.GetMethod("GetEnumerator");
-                object enumerator = getEnumeratorMethod.Invoke(report, null);
-                MethodInfo moveNextMethod = enumerator.GetType().GetMethod("MoveNext");
-                PropertyInfo currentProperty = enumerator.GetType().GetProperty("Current");
-                _ = (bool)moveNextMethod.Invoke(enumerator, null);
-                GetPropertiesOfObject(currentProperty, enumerator);
-            }
-            else
-            {
-                Console.WriteLine("The provided object is not an IEnumerable<T>.");
-            }
-        }
-
-        private void IterateGenericType<T>(T report, Type type)
-        {
-            // Get the IEnumerable<T> interface
-            Type enumerableType = typeof(IEnumerable<>).MakeGenericType(type.GetGenericArguments());
-
-            // Get the GetEnumerator() method
-            MethodInfo getEnumeratorMethod = enumerableType.GetMethod("GetEnumerator");
-
-            // Invoke the GetEnumerator() method to get the enumerator
-            object enumerator = getEnumeratorMethod.Invoke(report, null);
-
-            // Get the MoveNext() method and the Current property
-            MethodInfo moveNextMethod = enumerator.GetType().GetMethod("MoveNext");
-            PropertyInfo currentProperty = enumerator.GetType().GetProperty("Current");
-
-            // Iterate over the elements
-            while ((bool)moveNextMethod.Invoke(enumerator, null))
-            {
-                GetPropertiesOfObject(currentProperty, enumerator);
-            }
-        }
-
-        private static void GetPropertiesOfObject(PropertyInfo currentProperty, object enumerator)
-        {
-            object currentItem = currentProperty.GetValue(enumerator);
-
-            // Get the type of the current item
-            Type currentItemType = currentItem.GetType();
-
-            // Get all public properties of the current item
-            PropertyInfo[] properties = currentItemType.GetProperties();
-
-            Console.WriteLine($"Properties of the class {currentItemType.Name}:");
-
-            // Iterate over each property and print its value
-            foreach (PropertyInfo property in properties)
-            {
-                object propertyValue = property.GetValue(currentItem);
-
-                // Process each property value
-                Console.WriteLine($"Name: {property.Name} | Type: {property.PropertyType}");
-            }
         }
     }
 }
