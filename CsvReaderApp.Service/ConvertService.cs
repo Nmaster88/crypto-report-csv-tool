f951be9d7f5137aadea4d54dc9e4ba62c984eaa9
Nmaster88 <nunomaster21@gmail.com>
2023-07-13 20:55:11 +0100

    update

---
+++ CsvReaderApp.Service/ConvertService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReaderApp.Services
+{
+    public class ConvertService<TI,TO>
+    {
+        private readonly ObjectAssignementService<TI,TO> _objectAssignementService;
+        public ConvertService(ObjectAssignementService<TI, TO> objectAssignementService) 
+        { 
+            _objectAssignementService = objectAssignementService ?? throw new ArgumentNullException(nameof(ObjectAssignementService<TI, TO>));
+        }
+
+
+    }
+}
--- CsvReaderApp.Service/ObjectAssignementService.cs
+++ CsvReaderApp.Service/ObjectAssignementService.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+//using static CsvReaderApp.Services.ObjectAssignementService<TI, TO>;
 
 namespace CsvReaderApp.Services
 {
     public class ObjectAssignementService<TI, TO>
     {
         //TODO: console.writeLine or readLine should instead be replaced by DI. So that its generic.
-        private List<ObjectProperty> ObjectPropertiesI = new List<ObjectProperty>();
-        private List<ObjectProperty> ObjectPropertiesO = new List<ObjectProperty>();
+        public List<ObjectProperty> ObjectPropertiesI = new List<ObjectProperty>();
+        public List<ObjectProperty> ObjectPropertiesO = new List<ObjectProperty>();
+        public List<ObjectPropertiesMatch> ObjectPropertiesIOMatch = new List<ObjectPropertiesMatch>();
         
-        private class ObjectProperty
+        public class ObjectProperty
         {
             public required string Name { get; set; }
-            public required string Type { get; set; }
+            public required PropertyInfo Type { get; set; }
+            public required string TypeName { get; set; }
         }
-        
+
+        public class ObjectPropertiesMatch
+        {
+            public ObjectProperty PropertyInput { get; set; }
+            public ObjectProperty PropertyOutput { get; set; }
+            public ObjectConversion objectConversion { get; set; }
+        }
+
+        public enum ObjectConversion
+        {
+            NoConversion = 0,
+            StringToInt = 1,
+            StringToDecimal = 2,
+            IntToString = 3,
+            DecimalToString = 4,
+        }
+
         public void Setup()
         {
             GenericInputTypeRead<TI>();
@@ -60,7 +80,8 @@ namespace CsvReaderApp.Services
                     ObjectProperty objProperty = new ObjectProperty()
                     { 
                         Name = property.Name,
-                        Type = property.PropertyType.Name
+                        Type = property,
+                        TypeName = property.PropertyType.Name
                     };
                     ObjectProperties.Add(objProperty);
                 }
@@ -87,35 +108,105 @@ namespace CsvReaderApp.Services
             Console.WriteLine($"from the class {typeI} properties map each one of them to the class {typeO}");
 
             foreach(ObjectProperty propertyO in ObjectPropertiesO) {
-                Console.Write($"{propertyO.Name} match with: ");
                 ObjectProperty propertyI = null;
                 do
                 {
+                    Console.Write($"{propertyO.Name} match with: ");
                     string propName = Console.ReadLine();
                     propertyI = ObjectPropertiesI.Find(x => x.Name == propName);
-                    if(propertyI == null)
+
+                    if (propertyI == null)
                     {
                         Console.WriteLine("A property with that name was not found.");
                     }
-                    else if (propertyI?.Type == propertyO?.Type)
+                    else if (propertyI?.TypeName == propertyO?.TypeName)
                     {
                         Console.WriteLine("The type of the property is the same between them");
+                        AddObjectPropertiesMatch(propertyI, propertyO);
                     }
                     else
                     {
                         //TODO: In this case it should be possible to do a conversion?
                         //check both type of Input and Output and work on conversion
-                        CheckTypesAndApplyConvertion(propertyI?.Type, propertyO?.Type);
-                        Console.WriteLine("The type of the property is not the same");
+                        ObjectConversion objectConversion = CheckTypesAndGetConvertionType(propertyI.Type, propertyO.Type);
+                        Console.WriteLine("WARNING: The type of the properties is not the same");
+                        AddObjectPropertiesMatch(propertyI, propertyO, objectConversion);
                     }
                 }
                 while (propertyI == null);
             }
         }
 
-        private void CheckTypesAndApplyConvertion(string type1, string type2)
+        private void AddObjectPropertiesMatch(ObjectProperty propertyI, ObjectProperty propertyO, ObjectConversion objectConversion = ObjectConversion.NoConversion)
         {
+            ObjectPropertiesMatch objectPropertiesMatch = new ObjectPropertiesMatch();
+            objectPropertiesMatch.PropertyInput = propertyI;
+            objectPropertiesMatch.PropertyOutput = propertyO;
+            objectPropertiesMatch.objectConversion = objectConversion;
+            ObjectPropertiesIOMatch.Add(objectPropertiesMatch);
+        }
+
+        private ObjectConversion CheckTypesAndGetConvertionType(PropertyInfo infoI, PropertyInfo infoO)
+        {
+            Type typeI = infoI.PropertyType;
+            Type typeO = infoO.PropertyType;
+
+            if(typeI == typeof(int) && typeO == typeof(string))
+            {
+                return ObjectConversion.IntToString;
+            }
+
+            if (typeI == typeof(decimal) && typeO == typeof(string))
+            {
+                return ObjectConversion.DecimalToString;
+            }
+
+            if (typeI == typeof(string) && typeO == typeof(decimal))
+            {
+                return ObjectConversion.StringToDecimal;
+            }
+
+            if (typeI == typeof(string) && typeO == typeof(int))
+            {
+                return ObjectConversion.StringToInt;
+            }
             throw new NotImplementedException();
+        }
+
+        //TODO: code method that reads the value of a property and assigns it to the right method to convert
+
+        private string IntConvertToString(int value)
+        {
+            return value.ToString();
+        }
+
+        private string DecimalConvertToString(decimal value) {  
+            return value.ToString(); 
+        }
+
+        private decimal StringConvertToDecimal(string value)
+        {
+            decimal result;
+
+            if (decimal.TryParse(value, out result))
+            {
+                return result;
+            }
+
+            throw new ArgumentException("Invalid decimal value.");
+        }
+
+        private decimal StringConvertToInt(string value)
+        {
+            int intValue;
+            if (int.TryParse(value, out intValue))
+            {
+                return (decimal)intValue; // Successful conversion to int, cast to decimal
+            }
+
+            // Handle conversion failure or invalid input
+            // You can throw an exception, return a default value, or handle it based on your requirements
+            throw new ArgumentException("Invalid integer value.");
         }
     }
 }
