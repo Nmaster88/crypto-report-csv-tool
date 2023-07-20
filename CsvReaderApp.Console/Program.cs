66e5e91b81ed05ac8cccb52c1edfe06c634adf0d
Nmaster88 <nunomaster21@gmail.com>
2023-07-20 16:39:16 +0100

    add automapper to make process faster

--- CsvReaderApp.Console/Program.cs
+++ CsvReaderApp.Console/Program.cs
@@ -1,15 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AutoMapper;
 using Common.Services;
 using CsvReaderApp.Binance.Models;
+using CsvReaderApp.Console.Utils;
 using CsvReaderApp.Models;
 using CsvReaderApp.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var configuration = ConfigurationSetup();
 
+var serviceProvider = ConfigureServices();
+var mapper = serviceProvider.GetRequiredService<IMapper>();
+
 string directory = configuration.GetSection("AppSettings").GetValue<string>("Directory") ?? "C:\\Users\\Nuno\\Downloads";
 string fileName = configuration.GetSection("AppSettings").GetValue<string>("FileName") ?? "binance_report_2021.csv";
-
 Console.WriteLine("What is the Path for the file? Leave empty to use app settings value.");
 string inputFileName = Console.ReadLine();
 string filePath = !string.IsNullOrEmpty(inputFileName) ? inputFileName : Path.GetFullPath($"{directory}{Path.DirectorySeparatorChar}{fileName}");
@@ -19,19 +24,38 @@ IReader reader = new CsvReaderService();
 ReaderService readerService = new ReaderService(reader);
 var binanceReport = readerService.ReadRecords<BinanceReportEntry>(filePath);
 
+var destinationList = mapper.Map<List<AccountReportResult>>(binanceReport);
+
+Console.ReadLine();
 //TODO: study the possiblity to replace ObjectAssignementService and ConvertService with AutoMapper
-ObjectAssignementService<List<BinanceReportEntry>, List<AccountReportResult>> objectAssignmentService = new ObjectAssignementService<List<BinanceReportEntry>, List<AccountReportResult>>();
-objectAssignmentService.Setup();
-objectAssignmentService.Mapping();
+//ObjectAssignementService<List<BinanceReportEntry>, List<AccountReportResult>> objectAssignmentService = new ObjectAssignementService<List<BinanceReportEntry>, List<AccountReportResult>>();
+//objectAssignmentService.Setup();
+//objectAssignmentService.Mapping();
 
-ConvertService<List<BinanceReportEntry>, List<AccountReportResult>> convertService = new ConvertService<List<BinanceReportEntry>, List<AccountReportResult>>(objectAssignmentService);
-convertService.Convert(binanceReport, new List<AccountReportResult>());
+//ConvertService<List<BinanceReportEntry>, List<AccountReportResult>> convertService = new ConvertService<List<BinanceReportEntry>, List<AccountReportResult>>(objectAssignmentService);
+//convertService.Convert(binanceReport, new List<AccountReportResult>());
 
 
-static IConfiguration ConfigurationSetup()
+IConfiguration ConfigurationSetup()
 {
     var builder = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
         .AddJsonFile($"appsettings.json", true, true);
     return builder.Build();
+}
+
+IServiceProvider ConfigureServices()
+{
+    var services = new ServiceCollection();
+
+    var mapperConfig = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile<AutoMapperProfile>();
+    });
+
+    IMapper mapper = mapperConfig.CreateMapper();
+    services.AddSingleton(mapper);
+
+    var serviceProvider = services.BuildServiceProvider();
+    return serviceProvider;
 }
---
+++ CsvReaderApp.Console/Utils/AutoMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using CsvReaderApp.Binance.Models;
+using CsvReaderApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReaderApp.Console.Utils
+{
+    public class AutoMapperProfile : AutoMapper.Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<BinanceReportEntry, AccountReportResult>()
+                .ForMember(dest => dest.Change, opt => opt.MapFrom(src => decimal.Parse(src.Change, NumberStyles.Float | NumberStyles.AllowExponent, CultureInfo.InvariantCulture)));
+        }
+    }
+}
