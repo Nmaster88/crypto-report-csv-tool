432dbadbb14c4f3f173c441786be780d9e81395b
nmaster88 <nunomaster21@gmail.com>
2023-08-11 18:40:10 +0100

    .

--- CommonServicesTests/CsvWriterServiceTests.cs
+++ CommonServicesTests/CsvWriterServiceTests.cs
@@ -12,7 +12,7 @@ namespace CommonServicesTests
     public class CsvWriterServiceTests
     {
         private Common.Services.Interfaces.IWriter? _csvWriterService;
-        private readonly string _testFilePath = $"files{Path.DirectorySeparatorChar}testwrite.csv"; // Path to a test CSV file
+        private readonly string _testFilePath = $"files{Path.DirectorySeparatorChar}testwrite.csv"; // Maybe we dont want to create a real file
         private readonly string _expectedLine = "Sample Line";
         private readonly IStreamWriterWrapper? _streamWriterWrapper;
         private readonly IFileSystem _fileSystem;
@@ -28,18 +28,6 @@ namespace CommonServicesTests
 
             _streamWriterWrapper = Substitute.For<IStreamWriterWrapper>();
 
-            //_streamWriterWrapper
-            //    .WhenForAnyArgs(x => x.WriteLine(_expectedLine))
-            //    .Do(callInfo =>
-            //    {
-            //        // Call the action that was passed to WriteLine
-            //        var action = callInfo.Arg<Action<string>>();
-            //        action(_expectedLine);
-
-            //        // Call the equivalent WriteAllText method with the captured line and file path
-            //        _fileSystem.WriteAllText(_testFilePath, _expectedLine);
-            //    });
-
             var streamWriterWrapperFactoryMock = Substitute.For<IStreamWriterWrapperFactory>();
             streamWriterWrapperFactoryMock.Create(_testFilePath).Returns(_streamWriterWrapper);
 
@@ -49,7 +37,7 @@ namespace CommonServicesTests
         [TestInitialize]
         public void Initialize()
         {
-            this._csvWriterService = new CsvWriterService(_fileSystem, _streamWriterWrapperFactory, _testFilePath);
+            this._csvWriterService = new CsvWriterService(_streamWriterWrapperFactory, _testFilePath);
         }
 
         [TestCleanup]
@@ -67,8 +55,10 @@ namespace CommonServicesTests
             List<CsvWriterRecord> lines = new List<CsvWriterRecord>();
             lines.Add(new CsvWriterRecord() { Col1 = "Sample Line" });
 
-            //Assert
+            //Act
             _csvWriterService.WriteRecords(lines);
+            //mock for _streamWriterWrapper?
+
         }
 
         private void DeleteMockedFile()
