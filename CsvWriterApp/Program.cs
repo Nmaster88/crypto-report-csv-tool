aff08c4a98344b9a38d1421a5dcb4f3b381b5dab
nmaster88 <nunomaster21@gmail.com>
2023-08-11 18:37:12 +0100

    .

--- CsvWriterApp/Program.cs
+++ CsvWriterApp/Program.cs
@@ -1,26 +1,14 @@
 ﻿using Common.Services;
-using Common.Services.Interfaces;
 
 IStreamWriterWrapperFactory systemWriterWrapperFactory = new StreamWriterWrapperFactory();
 string filePath = $"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}nunog{Path.DirectorySeparatorChar}Downloads{Path.DirectorySeparatorChar}filetodelete.csv";
-//CsvWriterService csvWriterService = new CsvWriterService(systemWriterWrapperFactory, $"C:{Path.DirectorySeparatorChar}Users{Path.DirectorySeparatorChar}nunog{Path.DirectorySeparatorChar}Downloads{Path.DirectorySeparatorChar}filetodelete.csv");
-
-
-
-//List<CsvWriterRecord> lines = new List<CsvWriterRecord>();
-//lines.Add(new CsvWriterRecord() { Col1 = "Sample Line" });
 
 using (CsvWriterService csvWriterService = new CsvWriterService(systemWriterWrapperFactory, filePath))
 {
     List<CsvWriterRecord> lines = new List<CsvWriterRecord>();
     lines.Add(new CsvWriterRecord() { Col1 = "Sample Line" });
 
-    //List<string> lines = new List<string>();
-    //lines.Add("Sample Line");
-
     csvWriterService.WriteRecords(lines);
-
-    // No need to call csvWriterService.Dispose() here, using block will take care of it.
 }
 
 Console.WriteLine("CSV file written successfully.");
