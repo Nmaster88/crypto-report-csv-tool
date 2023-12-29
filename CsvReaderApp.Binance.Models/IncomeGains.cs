b71b1e77c37d652141243649f9415882cfe40590
Nmaster88 <nunomaster21@gmail.com>
2023-12-29 16:53:11 +0000

    .

---
+++ CsvReaderApp.Binance.Models/IncomeGains.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReaderApp.Binance.Models
+{
+    public record IncomeGains (
+        DateTime Date, 
+        string Asset, 
+        decimal Amount, 
+        decimal PricePerUnit /*Price per unit (EUR)*/,
+        decimal Value /*Value (EUR)*/,
+        string TransactionType /*Transaction Type*/,
+        string Label
+        );
+}
---
+++ CsvReaderApp.Binance.Models/Transactions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvReaderApp.Binance.Models
+{
+    public record Transactions (
+        string Id,
+        DateTime Date,
+        string Type,
+        string Label,
+        decimal SentAmount /*Sent Amount*/,
+        string SentCurrency /*Sent Currency*/,
+        string SentAddress /*Sent Address*/,
+        decimal ReceivedAmount /*Received Amount*/,
+        string ReceivedCurrency /*Received Currency*/,
+        decimal FeeAmount /*Fee Amount*/,
+        string FeeCurrency /*Fee Currency*/,
+        string Comment
+        );
+}
